be66bca0d0c90c67104261105b9ee315bfedb42a
Armeen Mahdian <mahdianarmeen@gmail.com>
2022-04-08 10:30:00 -0500

    fix: add default fontconfig config and create cache dirs

--- default.nix
+++ default.nix
@@ -5,25 +5,41 @@ let
     mkdir -p $out/tmp
   '';
 
+  fcCache = pkgs.runCommand "fcCache" {} ''
+    mkdir -p $out/var/cache/fontconfig
+  '';
+
 in nix2container.buildImage {
   inherit name tag;
 
   contents = [
     tmp
+    fcCache
+
     (pkgs.symlinkJoin {
       name = "root";
       paths = [ wolframengine ] ++ (with pkgs; [
         bash
+        coreutils
+        fontconfig.out
         ncurses
+        readline
       ]);
     })
   ];
 
-  perms = [ {
+  perms = [
+    {
       path = tmp;
       regex = ".*";
       mode = "0777";
-  } ];
+    }
+    {
+      path = fcCache;
+      reges = ".*";
+      mode = "0700";
+    }
+  ];
 
   config = {
     entrypoint = [ "/bin/WolframKernel" ];
